a32d9b2d4760f4d1e4541d0d29ced29634051aea
Buti <butifortune@gmail.com>
2025-06-26 20:56:42 +0200

    first

---
+++ manage-customer/manage-customer/src/test/java/com/bk/manage_customer/ManageCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bk.manage_customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
